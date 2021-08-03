19dbf65a0da265cb5eab68fcccbdd690ea7192c8
e195705 <e195705@ie.u-ryukyu.ac.jp>
2021-08-03 11:50:55 +0900

    Update MultiThread.py
    コード上書き
    対象の数字リストが全て素数だと見づらいため、一部合成数を含めた

--- MultiThread.py
+++ MultiThread.py
@@ -1,12 +1,28 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import math
+import time
 from concurrent import futures
 
-print( "Multi thread" )
-# 100000000000000003
-# 100000000000000013
-# 100000000000000019
-# 100000000000000021
-# 100000000000000049
+def search_prime(x):
+    if x % 2 == 0:
+        return False
+
+    for num in range(3, int(x**0.5) + 1, 2):
+        if x % num == 0:
+            return False
+
+    return True
+
+numbers = [10**18+3, 10**17+13, 10**17+19, 10**15+21, 10**14+49]
+numbers = [3,5,7,11,17]
+
+start = time.time()
+with futures.ThreadPoolExecutor() as executor:
+    mappings = {executor.submit(search_prime, n): n for n in numbers}
+    for future in futures.as_completed(mappings):
+        target = mappings[future]
+        result = future.result()
+        if result == True:
+            print("{n}:Prime Number".format(n=target))
+        else:
+            print("{n}:Not Prime Number".format(n=target))
+end = time.time()
+print(end - start)
